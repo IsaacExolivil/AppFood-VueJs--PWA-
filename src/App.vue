84797920833913a39f493e5e22b3db41422810ed
isaaclopez <exolivil@gmail.com>
2022-08-29 23:16:22 -0500

    add nav bar

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+ <div id="app">
+    <div class="navbar-container">
+      <div class="navbar-content">
+      <router-link to="/">
+        <i class="bx bx-home-heart bx-sm"></i>
+      </router-link>
+
+         <router-link to="/cart">
+        <i class="bx bx-cart bx-sm"></i>
+        </router-link>
+         <router-link to="/favoritos">
+        <i class='bx bxs-heart bx-sm' ></i>
+        </router-link>
+         <router-link to="/historial">
+        <i class='bx bx-history bx-sm' ></i>
+         </router-link>
+       </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+
+
+<style lang="less">
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap');
+.navbar-container {
+width: 100%;
+position: fixed;
+bottom: 0;
+  .navbar-content{
+    padding: 15px;
+    
+    display: flex;
+    justify-content: space-between;
+    a {
+      color: rgb(163, 163, 163);
+      &.router-link-exact-active {
+      color: rgb(255, 131, 62);
+    }
+    }
+    
+
+  }
+}
+
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+*{
+  margin:  0px;
+  padding: 0px;
+}
+
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <div  class="header">
+     <h2 class="title"> Hi <b>Isaac!</b></h2>
+     <div class="input">
+       <input type="text" placeholder="Find what you want...">
+       <div class="icon-input">
+        <i class="bx bx-search"></i>
+     </div>
+    
+     </div>
+    
+    </div>
+  </div>
+</template>
+
+
+<style lang="less">
+.home{
+  margin: 20px;
+}
+
+.header {
+  .input{
+    width: 100%;
+    input{
+      width: 100%;
+      background-color: rgb(250, 248, 248);
+      border: 0px;
+      outline: none;
+      padding: 10px;
+    }
+  }
+  .title{
+  font-weight: 350;
+}
+}
+</style>
+
