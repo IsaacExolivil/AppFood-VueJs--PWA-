4c53dd0533d8b22a3aa9f6b79e90b98ba576cc12
isaaclopez <exolivil@gmail.com>
2022-08-30 00:00:09 -0500

    add combo header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,20 @@
      <div class="input">
        <input type="text" placeholder="Find what you want...">
        <div class="icon-input">
+
+
         <i class="bx bx-search"></i>
+
+
+     
      </div>
+       
     
      </div>
+      <div class="combo-header">
+          <h3>Free combo</h3>
+          <p> Burger + Coca-cola for new users</p>
+        </div>
     
     </div>
   </div>
@@ -21,14 +31,30 @@
 }
 
 .header {
+  .combo-header{
+    
+    background-color: rgb(255, 131, 62);
+    color: white;
+    padding: 10px;
+  }
   .input{
-    width: 100%;
+    display: flex;
+    position: relative;
+    align-items: center;
+    margin-top: 20px;
     input{
+      
       width: 100%;
       background-color: rgb(250, 248, 248);
       border: 0px;
       outline: none;
       padding: 10px;
+      padding-left: 30px;
+      border-radius: 10px;
+    }
+    .icon-input{
+      position: absolute;
+      margin-left: 10px;
     }
   }
   .title{
